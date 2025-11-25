b0a287b6a24cf4a6797f2b4e1d37380551c32477
Mark Baiely <51993135+mrmebailey@users.noreply.github.com>
2025-11-25 10:23:47 +0000

    Updated run user

--- network_check.sh
+++ network_check.sh
@@ -3,7 +3,8 @@
 # Configuration
 TARGET="8.8.8.8"          # IP to ping (Google DNS). Use your router IP for local check.
 TIMEOUT=15                # Time to wait in seconds
-FALLBACK_SCRIPT="/home/mark/xmasjumper/cslm-christmas.py sqs" # Path to your fallback script
+FALLBACK_SCRIPT="/home/mark/xmasjumper/cslm-christmas.py" # Path to your fallback script
+MODE="sqs"
 
 echo "Starting network check..."
 
@@ -14,7 +15,7 @@ while [ $SECONDS -lt $end_time ]; do
     if ping -c 1 -W 1 "$TARGET" &> /dev/null; then
         echo "Network connection established."
         /usr/bin/git -C /home/mark/xmasjumper pull >> /home/mark/xmasjumper/git_reboot.log 2>&1
-        /usr/bin/python3 "$FALLBACK_SCRIPT"
+        /usr/bin/python3 "$FALLBACK_SCRIPT $MODE"
         exit 0
     fi
     sleep 1
